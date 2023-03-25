ee26f7fbcf771772875a01f740a41a0b54d9ee91
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-03-25 09:30:16 +0530

    Final COmmit

---
+++ Java/Abstract Class.java
@@ -0,0 +1,16 @@
+
+
+//Write MyBook class here
+class MyBook extends Book
+{
+    String title;
+    void setTitle(String s)
+    {
+        this.title = s;
+    }
+    String getTitle()
+    {
+        return title;
+    }
+}
+
