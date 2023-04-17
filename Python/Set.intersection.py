c5f1cb1b81c9c5ccf1a60c8e3ce4b3cdccca3caf
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-04-17 17:25:34 +0530

    Create Set.intersection.py

---
+++ Python/Set.intersection.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+M = int(input())
+set1 = set(map(int,input().split()))
+N = int(input())
+set2 = set(map(int,input().split()))
+
+set3 = set1.intersection(set2)
+
+print(len(set3))
