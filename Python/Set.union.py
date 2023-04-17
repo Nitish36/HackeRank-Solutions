2c2d3cbc86cc6583ddc3499b667a840a158d311d
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-04-17 17:22:56 +0530

    Create Set.union.py

---
+++ Python/Set.union.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input())
+set1 = set(map(int,input().split()))
+M = int(input())
+set2 = set(map(int,input().split()))
+
+set3 = set1.union(set2)
+
+print(len(set3))
